28260f687162961e145789a1e4c35ac38e1777e5
Julian Schmuckli <julian@schmuckli.net>
2019-10-07 00:40:07 +0200

    Improved project structure

--- app/schmucklicloud_service.js
+++ app/schmucklicloud_service.js
@@ -7,6 +7,6 @@ const app_secret = "66d7549b440fe6b36287f5f8800f68cd681a6c6439f3915040e60f9a06e0
 let ref_auth = new sCAuth(app_id, app_secret);
 let ref_storage = new sCStorage(app_id, app_secret);
 
-storage.setBucket(16);
+ref_storage.setBucket(16);
 
 export {ref_auth as auth, ref_storage as storage};
